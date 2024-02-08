2c63964aa5566b2ce4f12fa0e5045e09b23f5af0
Lionel Cons <lionel.cons@cern.ch>
2024-02-08 12:54:19 +0100

    updated the copyright dates

--- src/main/java/ch/cern/dirq/test/TestDirq.java
+++ src/main/java/ch/cern/dirq/test/TestDirq.java
@@ -28,7 +28,7 @@ import ch.cern.dirq.QueueSimple;
  *
  * @author Lionel Cons &lt;lionel.cons@cern.ch&gt;
  * @author Massimo Paladin &lt;massimo.paladin@gmail.com&gt;
- * Copyright (C) CERN 2012-2020
+ * Copyright (C) CERN 2012-2024
  */
 public class TestDirq {
 
